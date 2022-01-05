d81ac1ccee6f533d90e0831ac45acf3a4118be28
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-01-05 10:47:26 +0100

    turn into functional

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,33 +1,23 @@
-import React, {Component} from 'react';
-
 import {getProjects} from '../data';
 import {LinkContainer} from 'react-router-bootstrap';
 import {Nav} from 'react-bootstrap';
+import React from 'react';
 
-class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const projects = getProjects();
-    return (
-      <React.Fragment>
-        <h2>Projects</h2>
-        <Nav>
-          {
-            projects.map((project, projectIdx) =>
-              <Nav.Item key={projectIdx} as="nav">
-                <LinkContainer to={`/projects/${project.name}`} key={`${project.name}`}>
-                  <Nav.Link>{`${project.name}`}</Nav.Link>
-                </LinkContainer>
-              </Nav.Item>
-            )}
-        </Nav>
-      </React.Fragment>
-    );
-  }
+export default function Projects() {
+  const projects = getProjects();
+  return (
+    <React.Fragment>
+      <h2>Projects</h2>
+      <Nav as="ul">
+        {
+          projects.map((project, projectIdx) =>
+            <Nav.Item key={projectIdx} as="li">
+              <LinkContainer to={`/projects/${project.name}`} key={`${project.name}`}>
+                <Nav.Link>{`${project.name}`}</Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+          )}
+      </Nav>
+    </React.Fragment>
+  );
 }
-
-export default Projects;
